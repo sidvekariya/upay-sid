223048f94c80fd0b3d53ef249ea6933ce4631fcc
Sid <sidvekariya20@gmail.com>
2023-09-01 09:30:22 +0530

    incomplete navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,47 @@
 import React from "react";
+import styles from "../styles/navbar.module.css";
 import Wrapper from "./Wrapper";
-import localFont from "next/font/local";
+import Image from "next/image";
+import logo from "../public/images/navbar-images/upayLogo.svg";
+import { AiOutlineUserAdd } from "react-icons/ai";
+import { HiOutlineBars3BottomRight } from "react-icons/hi";
 
-const myFont = localFont({ src: "../fonts/Nexa Bold.otf" });
+import localFont from "next/font/local";
+import { Mulish } from "@next/font/google";
+const mulish = Mulish({ subsets: ["latin"] });
+const nexaBold = localFont({ src: "../fonts/Nexa Bold.otf" });
+const nexaRegular = localFont({ src: "../fonts/Nexa Regular.otf" });
 
 const Navbar = () => {
     return (
-        <Wrapper>
-            <div style={myFont.style}>navbar</div>
+        <Wrapper background="green">
+            <div className={styles.navbar}>
+                <div className={styles.logoText}>
+                    <Image className={styles.upayLogo} src={logo} alt="logo" />
+                    <div style={mulish.style} className={styles.text}>
+                        upay
+                    </div>
+                </div>
+                <ul className={styles.ul}>
+                    <li>Home</li>
+                    <li>Services</li>
+                    <li>Pricing</li>
+                    <li>About Us</li>
+                    <li>Contact</li>
+                </ul>
+                <div
+                    style={nexaRegular.style}
+                    className={styles.loginAndSignup}
+                >
+                    <div className={styles.login}>Log in</div>
+                    <div className={styles.signupLogo}>
+                        <div className={styles.signUp}>Sign up</div>
+                        <div className={styles.logo}>
+                            <AiOutlineUserAdd />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Wrapper>
     );
 };
